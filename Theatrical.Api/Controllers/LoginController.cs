38221010cf0c7faffae0a2762f71b8556bd668a6
nikosv <nikosv@arx.net>
2023-05-12 01:24:10 +0300

    Experimental Login Controller

---
+++ Theatrical.Api/Controllers/LoginController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Theatrical.Dto.LoginDtos;
+
+namespace Theatrical.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class LoginController : ControllerBase
+{
+
+    [HttpPost]
+    public IActionResult Login([FromBody]UserLoginDto userLoginDto)
+    {
+        return Ok();
+    }
+}
