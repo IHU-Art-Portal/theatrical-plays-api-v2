e0d632d28be19508711536cee38c85fc438dd554
nikosv <nikosv@arx.net>
2023-05-12 00:08:32 +0300

    Initial version of controller

---
+++ Theatrical.Api/Controllers/PerformersController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Theatrical.Api.Controllers;
+
+[ApiController]
+[Route("/api/[controller]")]
+public class PerformersController : ControllerBase
+{
+
+    [HttpGet]
+    [Route("{id:int}")]
+    public IActionResult GetPerformer(int id)
+    {
+        return Ok();
+    }
+
+    [HttpGet]
+    public IActionResult GetPerformers(int? page, int? size)
+    {
+        return Ok();
+    }
+}
