6cae15227cd06b5a774040fad32849c6322a6de8
nikosv <nikosv@arx.net>
2023-08-10 17:29:25 +0300

    Logs the created transaction.

--- Theatrical.Services/Repositories/TransactionRepository.cs
+++ Theatrical.Services/Repositories/TransactionRepository.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System.Globalization;
+using Microsoft.EntityFrameworkCore;
 using Theatrical.Data.Context;
 using Theatrical.Data.Models;
 
@@ -14,16 +15,27 @@ public interface ITransactionRepository
 public class TransactionRepository : ITransactionRepository
 {
     private readonly TheatricalPlaysDbContext _context;
+    private readonly ILogRepository _logRepository;
 
-    public TransactionRepository(TheatricalPlaysDbContext context)
+    public TransactionRepository(TheatricalPlaysDbContext context, ILogRepository logRepository)
     {
         _context = context;
+        _logRepository = logRepository;
     }
 
     public async Task<Transaction> PostTransaction(Transaction transaction)
     {
         await _context.Transactions.AddAsync(transaction);
         await _context.SaveChangesAsync();
+
+        await _logRepository.UpdateLogs("insert", "transactions", new List<(string ColumnName, string Value)>
+        {
+            ("Id", transaction.Id.ToString()),
+            ("UserId", transaction.UserId.ToString()),
+            ("CreditAmount", transaction.CreditAmount.ToString(CultureInfo.CurrentCulture)),
+            ("Reason", transaction.Reason)
+        });
+
         return transaction;
     }
     
