23b2cc5647a043d555d0dd4fa090fa4df083fe4c
Nick-v1 <nikos.voulgaridis1@gmail.com>
2023-12-07 17:46:23 +0200

    minio service update. Files are sorted in folders inside the main bucket.

--- Theatrical.Services/MinioService.cs
+++ Theatrical.Services/MinioService.cs
@@ -8,11 +8,12 @@ namespace Theatrical.Services;
 public interface IMinioService
 {
     Task<string> PostPdf(byte[] pdfBytes, int userId);
+    Task<string> PostUserBioPdf(byte[] pdfBytes, int userId);
 }
 
 public class MinioService : IMinioService
 {
-    private string _bucketName = "api.testing";
+    private string _bucketName = "test.dot.net.api";
     private readonly IMinioClient minioClient;
 
     public MinioService(IMinioClient minioClient)
@@ -35,7 +36,7 @@ public class MinioService : IMinioService
             );
         }
         
-        var objectName = $"IdentificationDocument{userId}.pdf";
+        var objectName = $"UserAccountRequests/IdentificationDocument{userId}.pdf";
         ReadOnlyMemory<byte> bs = new ReadOnlyMemory<byte>(pdfBytes);
 
         using var fileStream = bs.AsStream();
@@ -70,5 +71,37 @@ public class MinioService : IMinioService
         var url = await minioClient.PresignedGetObjectAsync(presignedArgs);
         return url;
     }
+     
+    public async Task<string> PostUserBioPdf(byte[] pdfBytes, int userId)
+    {
+        var existArg = new BucketExistsArgs().WithBucket(_bucketName);
+        var found = await minioClient.BucketExistsAsync(existArg);
+        
+        //Creates the bucket if not exists.
+        if (!found)
+        {
+            await minioClient.MakeBucketAsync(
+                new MakeBucketArgs()
+                    .WithBucket(_bucketName)
+            );
+        }
+        
+        var objectName = $"UserBiographies/User_{userId}_Bio.pdf";
+        ReadOnlyMemory<byte> bs = new ReadOnlyMemory<byte>(pdfBytes);
+
+        using var fileStream = bs.AsStream();
+
+        var putArgs = new PutObjectArgs()
+            .WithBucket(_bucketName)
+            .WithObject(objectName)
+            .WithStreamData(fileStream)
+            .WithObjectSize(fileStream.Length)
+            .WithContentType("application/pdf");
+        
+        await minioClient.PutObjectAsync(putArgs);
+
+        return await GetDownloadLink(objectName);
+    }
+    
     
 }
